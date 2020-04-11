4d869d26292709539e578b821a0ce825d840e44a
Kyle Bashour <kylebshr@gmail.com>
2020-04-10 17:38:32 -0700

    Fix overscroll position calculation (#112)
    Co-authored-by: Kyle Bashour <kbashour@squareup.com>

--- Listable/Sources/ListView/ListView.swift
+++ Listable/Sources/ListView/ListView.swift
@@ -814,25 +814,8 @@ extension ListView : KeyboardObserverDelegate
     }
 }
 
-
-
 fileprivate extension UIScrollView
 {
-
-    /// The frame of the collection view inset by the adjusted content inset,
-    /// i.e., the visible frame of the content.
-    var contentFrame : CGRect {
-        return self.bounds.inset(by: self.lst_adjustedContentInset)
-    }
-
-    /// `adjustedContentInset` on iOS >= 11, `contentInset` otherwise.
-    var lst_adjustedContentInset : UIEdgeInsets {
-        if #available(iOS 11, *) {
-            return self.adjustedContentInset
-        } else {
-            return self.contentInset
-        }
-    }
 
     func isScrolledNearBottom() -> Bool
     {
