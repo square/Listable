a9fd056a872f5b712cae5f40c3808cc68f735788
Kyle Bashour <kbashour@squareup.com>
2020-05-20 11:21:02 -0700

    Export Listable in BlueprintLists

---
+++ BlueprintLists/Sources/BlueprintListsModule.swift
@@ -0,0 +1,10 @@
+//
+//  BlueprintListsModule.swift
+//  BlueprintLists
+//
+//  Created by Kyle Bashour on 5/20/20.
+//
+
+/// Since many of BlueprintLists types use Listable types, we export the public API of Listable
+/// when importing BlueprintLists.
+@_exported import Listable
