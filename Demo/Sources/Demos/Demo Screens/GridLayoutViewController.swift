ebc8ff91a12ea2ea7beee4be295ab8a41e4276b6
Kyle Van Essen <k@squareup.com>
2020-08-09 16:30:30 -0700

    Use listviewcontroller

--- Demo/Sources/Demos/Demo Screens/GridLayoutViewController.swift
+++ Demo/Sources/Demos/Demo Screens/GridLayoutViewController.swift
@@ -14,30 +14,23 @@ import BlueprintUI
 import BlueprintUICommonControls
 
 
-final class GridLayoutViewController : UIViewController
+final class GridLayoutViewController : ListViewController
 {
-    let listView = ListView()
-    
-    override func loadView()
-    {
-        self.view = self.listView
+    override func configure(list: inout ListProperties) {
+        
+        list.appearance = .demoAppearance
+        
+        list.layout = .grid {
+            $0.layout.padding = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
+        }
+        
+        list += (1...10).map { sectionIndex in
+            Section(sectionIndex) { section in
                 
-        self.listView.configure { list in
-            
-            list.appearance = .demoAppearance
-            
-            list.layout = .grid {
-                $0.layout.padding = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
-            }
-            
-            list += (1...10).map { sectionIndex in
-                Section(sectionIndex) { section in
-                    
-                    section.header = HeaderFooter(DemoHeader(title: "Section \(sectionIndex)"))
-                    
-                    section += (1...sectionIndex).map { rowIndex in
-                        DemoItem(text: "Row \(rowIndex)")
-                    }
+                section.header = HeaderFooter(DemoHeader(title: "Section \(sectionIndex)"))
+                
+                section += (1...sectionIndex).map { rowIndex in
+                    DemoItem(text: "Row \(rowIndex)")
                 }
             }
         }
