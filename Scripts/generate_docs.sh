b5be351e312c4f1842ba10dba11c677d5ffd3a70
Kyle Van Essen <k@squareup.com>
2020-09-06 19:24:30 -0700

    more

--- Scripts/generate_docs.sh
+++ Scripts/generate_docs.sh
@@ -10,16 +10,9 @@ platform="iOS Simulator"
 device=`instruments -s -devices | grep -oE 'iPhone.*?[^\(]+' | head -1 | awk '{$1=$1;print}'`
 destination="platform=$platform,name=$device"
 
-$sourcekitten doc \
-  --module-name Listable \
-  -- \
-  -scheme "Demo" -destination "$destination" \
-  > Listable.json
+$sourcekitten doc -- -workspace Demo/Demo.xcworkspace -scheme Listable -destination "$destination" > Listable.json
 
-$sourcekitten doc \
-  --module-name BlueprintLists \
-  -- \
-  -scheme "Demo" -destination "$destination" \
-  > BlueprintLists.json
+$sourcekitten doc -- -workspace Demo/Demo.xcworkspace -scheme BlueprintLists -destination "$destination" > BlueprintLists.json
 
-jazzy --sourcekitten-sourcefile Listable.json,BlueprintLists.json
+jazzy --sourcekitten-sourcefile Listable.json --output docs/listable
+jazzy --sourcekitten-sourcefile BlueprintLists.json --output docs/blueprintlists
