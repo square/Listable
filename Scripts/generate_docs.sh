855621a7735421b90f4dd30d475a489a1ddf1ade
Kyle Van Essen <k@squareup.com>
2020-09-06 19:48:52 -0700

    more

--- Scripts/generate_docs.sh
+++ Scripts/generate_docs.sh
@@ -6,13 +6,11 @@ set -euxo pipefail
 # Find the sourcekitten binary embedded in jazzy
 sourcekitten=`gem contents jazzy | grep 'bin/sourcekitten$' | head -1`
 
-platform="iOS Simulator"
-device=`instruments -s -devices | grep -oE 'iPhone.*?[^\(]+' | head -1 | awk '{$1=$1;print}'`
-destination="platform=$platform,name=$device"
+destination="platform=iOS Simulator,name=iPhone 11"
 
 $sourcekitten doc -- -workspace Demo/Demo.xcworkspace -scheme Listable -destination "$destination" > Listable.json
 
 $sourcekitten doc -- -workspace Demo/Demo.xcworkspace -scheme BlueprintLists -destination "$destination" > BlueprintLists.json
 
-jazzy --sourcekitten-sourcefile Listable.json --output docs/listable
-jazzy --sourcekitten-sourcefile BlueprintLists.json --output docs/blueprintlists
+jazzy --sourcekitten-sourcefile Listable.json --output docs/Listable --module "Listable"
+jazzy --sourcekitten-sourcefile BlueprintLists.json --output docs/BlueprintLists --module "BlueprintLists"
