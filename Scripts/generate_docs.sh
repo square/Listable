b3e5a0ad141f4880e53467d7d03ff693196fedd8
Kyle Van Essen <k@squareup.com>
2020-09-06 19:56:37 -0700

    more

--- Scripts/generate_docs.sh
+++ Scripts/generate_docs.sh
@@ -9,8 +9,14 @@ sourcekitten=`gem contents jazzy | grep 'bin/sourcekitten$' | head -1`
 destination="platform=iOS Simulator,name=iPhone 11"
 
 $sourcekitten doc -- -workspace Demo/Demo.xcworkspace -scheme Listable -destination "$destination" > Listable.json
-
 $sourcekitten doc -- -workspace Demo/Demo.xcworkspace -scheme BlueprintLists -destination "$destination" > BlueprintLists.json
 
-jazzy --sourcekitten-sourcefile Listable.json --output docs/Listable --module "Listable"
-jazzy --sourcekitten-sourcefile BlueprintLists.json --output docs/BlueprintLists --module "BlueprintLists"
+jazzy \
+	--config docs/listable.yml
+	--sourcekitten-sourcefile Listable.json \
+	--output docs/Listable \
+
+jazzy \
+	--config docs/blueprintlists.yml
+	--sourcekitten-sourcefile BlueprintLists.json \
+	--output docs/BlueprintLists \
