38e700acd3eef39381d1d7e951638ba34e0fe042
Kyle Van Essen <k@squareup.com>
2020-12-09 11:08:02 -0800

    test

--- Scripts/run_ios11_tests.sh
+++ Scripts/run_ios11_tests.sh
@@ -11,13 +11,13 @@ xcrun simctl delete "$SIMULATOR_NAME" || true
 
 # Create New Simulators
 
-DEVICE_UUID=$(xcrun simctl create "$SIMULATOR_NAME" "iPhone 7" "com.apple.CoreSimulator.SimRuntime.iOS-11-1")
-echo "Created iOS 11 simulator ($SIMULATOR_NAME): $DEVICE_UUID"
+# DEVICE_UUID=$(xcrun simctl create "$SIMULATOR_NAME" "iPhone 7" "com.apple.CoreSimulator.SimRuntime.iOS-11-1")
+# echo "Created iOS 11 simulator ($SIMULATOR_NAME): $DEVICE_UUID"
 
-xcrun simctl boot "$DEVICE_UUID"
+# xcrun simctl boot "$DEVICE_UUID"
 
 
-# Run Build
+# # Run Build
 
-xcodebuild build-for-testing -workspace "Demo/Demo.xcworkspace" -scheme "Demo" -destination "id=$DEVICE_UUID" -quiet
-xcodebuild test-without-building -workspace "Demo/Demo.xcworkspace" -scheme "Demo" -destination "id=$DEVICE_UUID"
+# xcodebuild build-for-testing -workspace "Demo/Demo.xcworkspace" -scheme "Demo" -destination "id=$DEVICE_UUID" -quiet
+# xcodebuild test-without-building -workspace "Demo/Demo.xcworkspace" -scheme "Demo" -destination "id=$DEVICE_UUID"
