986e511c11432d5389eddd65e8a5c6e917addf71
Kyle Van Essen <k@squareup.com>
2021-11-23 13:32:09 -0800

    Allow accessing standard properties directly from LayoutDescription.

--- ListableUI/Sources/Layout/ListLayout/ListLayoutAppearance.swift
+++ ListableUI/Sources/Layout/ListLayout/ListLayoutAppearance.swift
@@ -16,3 +16,25 @@ public protocol ListLayoutAppearance : Equatable
     
     var stickySectionHeaders : Bool { get }
 }
+
+
+/// Represents the properties from a `ListLayoutAppearance`, which
+/// are applicable to any kind of layout.
+public struct ListLayoutAppearanceProperties : Equatable {
+        
+    public let direction : LayoutDirection
+    public let stickySectionHeaders : Bool
+    
+    public init(
+        direction: LayoutDirection,
+        stickySectionHeaders: Bool
+    ) {
+        self.direction = direction
+        self.stickySectionHeaders = stickySectionHeaders
+    }
+    
+    public init<Appearance:ListLayoutAppearance>(_ appearance : Appearance) {
+        self.direction = appearance.direction
+        self.stickySectionHeaders = appearance.stickySectionHeaders
+    }
+}
