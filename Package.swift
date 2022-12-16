20fffefda862313726883b2e68d476228faee4ed
Adam Lickel <adam@lickel.com>
2022-12-16 11:36:30 -0800

    SPM dependency minimum

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/square/Blueprint", from: "0.47.0"),
+        .package(url: "https://github.com/square/Blueprint", from: "0.48.0"),
     ],
     targets: [
         .target(
