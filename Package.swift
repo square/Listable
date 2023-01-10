85af6bf59fabbfeeba6f31882aa4817d8a50f102
Kyle Van Essen <k@squareup.com>
2023-01-10 13:30:26 -0800

    Update Blueprint dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/square/Blueprint", from: "0.48.0"),
+        .package(url: "https://github.com/square/Blueprint", from: "0.48.1"),
     ],
     targets: [
         .target(
