3bf4b5119a67882daa25037ed63ee8a65f688bec
Robert MacEachern <robmaceachern@squareup.com>
2023-05-12 14:13:40 -0500

    Added read-only dynamic member lookup support to ListProperties (#484)

--- ListableUI/Tests/ListPropertiesTests.swift
+++ ListableUI/Tests/ListPropertiesTests.swift
@@ -5,9 +5,37 @@
 //  Created by Kyle Van Essen on 11/27/19.
 //
 
+import ListableUI
 import XCTest
 
 class ListPropertiesTests : XCTestCase
 {
-
+    private func properties() -> ListProperties {
+        ListProperties(
+            animatesChanges: true,
+            layout: .flow(),
+            appearance: .init(),
+            scrollIndicatorInsets: .zero,
+            behavior: .init(),
+            autoScrollAction: .none,
+            accessibilityIdentifier: "",
+            debuggingIdentifier: "") { _ in }
+    }
+    
+    func test_read_only_dynamic_member_lookup() {
+        let properties = properties()
+        // Ensure that this compiles. Previously we were missing the read-only KeyPath
+        // dynamicMember subscript implementation which would lead to this error:
+        // Cannot assign to property: 'lastItem' is a get-only property
+        let item = properties.lastItem
+        XCTAssertNil(item)
+    }
+    
+    func test_writeable_dynamic_member_lookup() {
+        var properties = properties()
+        // Ensure that this compiles and writes through to the underlying content.
+        properties.identifier = "Hello"
+        XCTAssertEqual(properties.identifier, "Hello")
+        XCTAssertEqual(properties.identifier, properties.content.identifier)
+    }
 }
