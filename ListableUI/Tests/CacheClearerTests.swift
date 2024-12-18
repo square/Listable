fc39dfdc83616dd2b9ac730bc96626643beba050
Robert MacEachern <robmaceachern@squareup.com>
2024-12-18 11:59:18 -0600

    More flexible CacheClearerTests to account for full suite run that may have pre-populated the caches

--- ListableUI/Tests/CacheClearerTests.swift
+++ ListableUI/Tests/CacheClearerTests.swift
@@ -8,8 +8,8 @@ class CacheClearerTests: XCTestCase {
         ListProperties.headerFooterMeasurementCache.push(UIView(), with: .identifier(for: UIView.self))
         ListProperties.itemMeasurementCache.push(UIView(), with: .identifier(for: UIView.self))
 
-        XCTAssertEqual(ListProperties.headerFooterMeasurementCache.cachedViewCount, 1)
-        XCTAssertEqual(ListProperties.itemMeasurementCache.cachedViewCount, 1)
+        XCTAssertGreaterThanOrEqual(ListProperties.headerFooterMeasurementCache.cachedViewCount, 1)
+        XCTAssertGreaterThanOrEqual(ListProperties.itemMeasurementCache.cachedViewCount, 1)
 
         CacheClearer.clearStaticCaches()
         
