d0590eea61f4c49f35155c6f2d7da092e565e17d
Meher Kasam <meher@squareup.com>
2025-07-23 10:47:33 +0530

    Update Tuist/Package.swift

--- Development/Tuist/Package.swift
+++ Development/Tuist/Package.swift
@@ -2,6 +2,31 @@
 
 import PackageDescription
 
+#if TUIST
+import ProjectDescription
+
+let packageSettings = PackageSettings(
+    productTypes: [
+        "BlueprintUI": .framework,
+        "BlueprintUICommonControls": .framework,
+        "BlueprintUILists": .framework,
+        "ListableUI": .framework,
+    ],
+    targetSettings: [
+        "BlueprintUILists": unsuppressedWarningsSettings,
+        "ListableUI": unsuppressedWarningsSettings,
+    ]
+)
+
+var unsuppressedWarningsSettings: SettingsDictionary {
+    [
+        "GCC_WARN_INHIBIT_ALL_WARNINGS": "NO",
+        "SWIFT_SUPPRESS_WARNINGS": "NO",
+    ]
+}
+
+#endif
+
 let package = Package(
     name: "Development",
     dependencies: [
